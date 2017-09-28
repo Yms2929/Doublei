2e950bd84ba12b6880a6f63d8825539f2a79dddd
Yookmoonsu <yookmoon@naver.com>
2017-09-28 18:52:25 +0900

    달력 특정날짜 효과및 클릭이벤트 추가

--- Doublei/app/src/main/java/com/example/doublei/Setting/PreviousRecord.java
+++ Doublei/app/src/main/java/com/example/doublei/Setting/PreviousRecord.java
@@ -1,10 +1,24 @@
 package com.example.doublei.Setting;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StyleSpan;
+import android.widget.Toast;
 
 import com.example.doublei.R;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.DayViewDecorator;
+import com.prolificinteractive.materialcalendarview.DayViewFacade;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class PreviousRecord extends AppCompatActivity {
 
@@ -15,5 +29,77 @@ public class PreviousRecord extends AppCompatActivity {
 
         MaterialCalendarView calendarView = (MaterialCalendarView) findViewById(R.id.calendarView);
 
+        calendarView.addDecorators(new SundayDecorator(), new SaturdayDecorator(), new OneDayDecorator());
+
+        calendarView.setOnDateChangedListener(new OnDateSelectedListener() { // 달력 클릭 이벤트
+            @Override
+            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+                Toast.makeText(getApplicationContext(), String.valueOf(date.getMonth() + 1) + "월" + String.valueOf(date.getDay()) + "일", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public class SundayDecorator implements DayViewDecorator { // 일요일
+        private final Calendar calendar = Calendar.getInstance();
+
+        public SundayDecorator() {
+
+        }
+
+        @Override
+        public boolean shouldDecorate(CalendarDay day) {
+            day.copyTo(calendar);
+            int weekDay = calendar.get(Calendar.DAY_OF_WEEK);
+            return weekDay == Calendar.SUNDAY;
+        }
+
+        @Override
+        public void decorate(DayViewFacade view) {
+            view.addSpan(new ForegroundColorSpan(Color.RED));
+        }
+    }
+
+    public class SaturdayDecorator implements DayViewDecorator { // 토요일
+        private final Calendar calendar = Calendar.getInstance();
+
+        public SaturdayDecorator() {
+
+        }
+
+        @Override
+        public boolean shouldDecorate(CalendarDay day) {
+            day.copyTo(calendar);
+            int weekDay = calendar.get(Calendar.DAY_OF_WEEK);
+            return weekDay == Calendar.SATURDAY;
+        }
+
+        @Override
+        public void decorate(DayViewFacade view) {
+            view.addSpan(new ForegroundColorSpan(Color.BLUE));
+        }
+    }
+
+    public class OneDayDecorator implements DayViewDecorator { // 오늘 날짜
+        private CalendarDay date;
+
+        public OneDayDecorator() {
+            date = CalendarDay.today();
+        }
+
+        @Override
+        public boolean shouldDecorate(CalendarDay day) {
+            return date != null && day.equals(date);
+        }
+
+        @Override
+        public void decorate(DayViewFacade view) {
+            view.addSpan(new StyleSpan(Typeface.BOLD));
+            view.addSpan(new RelativeSizeSpan(1.4f));
+            view.addSpan(new ForegroundColorSpan(Color.GREEN));
+        }
+
+        public void setDate(Date date) {
+            this.date = CalendarDay.from(date);
+        }
     }
 }
