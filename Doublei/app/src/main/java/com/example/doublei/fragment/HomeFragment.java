8660c31ecad6c59443494fb54aa3171ffa0d2b7a
DESKTOP-7IL07IJ\Taegyun <93taegyun@gmail.com>
2017-09-30 14:52:28 +0900

    사용시간 (초 단위)로 SharedPreference에 구현. UI 단으로 옮겨갈 때 시,분,초 단위로 변환해서 제공해야함. MM월 DD일 순으로 Key 값 설정되어있음.

--- Doublei/app/src/main/java/com/example/doublei/fragment/HomeFragment.java
+++ Doublei/app/src/main/java/com/example/doublei/fragment/HomeFragment.java
@@ -8,6 +8,7 @@ import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,11 +21,17 @@ import com.example.doublei.Etc.Singleton;
 import com.example.doublei.Etc.Widget;
 import com.example.doublei.R;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class HomeFragment extends Fragment {
     public ImageButton btnEye;
     public Button btnConnect;
     public boolean value;
     public int appWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
+    public long startTime, endTime, betweenTime;
+    public int accumulateTime;
+
 
     public HomeFragment() {
         // Required empty public constructor
@@ -67,11 +74,34 @@ public class HomeFragment extends Fragment {
             Toast.makeText(getActivity().getApplicationContext(), "백그라운드 작업이 종료됩니다.", Toast.LENGTH_LONG).show();
             value = false;
             Singleton.getInstance().setSwitchValue(value);
-
+            // 위젯을 위한 SharedPreference
             SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
             SharedPreferences.Editor editor = prefs.edit();
             editor.putBoolean("widgetOn", false);
             editor.commit();
+
+            // 시간 계산을 위한 SharedPreference
+            // 타임 종료
+
+            endTime = System.currentTimeMillis();
+            betweenTime = (endTime - startTime)/1000; // 초
+
+            long date = System.currentTimeMillis(); // 시간받아오기
+            Date nowDate = new Date(date); // Date 타입 변경
+            SimpleDateFormat sdf = new SimpleDateFormat("MM월 dd일"); // 월, 일 받아오기
+            String dateTemp = sdf.format(nowDate);
+
+            SharedPreferences timePref = this.getActivity().getSharedPreferences(dateTemp,Context.MODE_PRIVATE);
+            SharedPreferences.Editor timeEditor = timePref.edit();
+            String checkDate = timePref.getString(dateTemp,""); // key 값으로 dateTemp를 주고 그에 해당하는 Value를 가져옴. 없으면 공백("")
+
+            accumulateTime = Integer.valueOf(checkDate);
+            accumulateTime += Integer.valueOf(String.valueOf(betweenTime));
+
+            timeEditor.putString(dateTemp, String.valueOf(accumulateTime));
+            timeEditor.commit();
+            String test = timePref.getString(dateTemp,"");
+            Log.e("test",test);
 
             Widget.updateWidget(context, widgetManager, appWidgetId);
 
@@ -85,10 +115,41 @@ public class HomeFragment extends Fragment {
             value = true;
             Singleton.getInstance().setSwitchValue(value);
 
+            // 위젯을 위한 SharedPreference
             SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
             SharedPreferences.Editor editor = prefs.edit();
             editor.putBoolean("widgetOn", true);
             editor.commit();
+
+            // 시간 계산을 위한 SharedPreference
+            long date = System.currentTimeMillis(); // 시간받아오기
+            Date nowDate = new Date(date); // Date 타입 변경
+            SimpleDateFormat sdf = new SimpleDateFormat("MM월 dd일"); // 월, 일 받아오기
+            String dateTemp = sdf.format(nowDate);
+
+            SharedPreferences timePref = this.getActivity().getSharedPreferences(dateTemp,Context.MODE_PRIVATE);
+            SharedPreferences.Editor timeEditor = timePref.edit();
+            String checkDate = timePref.getString(dateTemp,""); // key 값으로 dateTemp를 주고 그에 해당하는 Value를 가져옴. 없으면 공백("")
+            // 오늘 첫 작동인 경우
+            if(checkDate.equals("")){
+                timeEditor.putString(dateTemp,"0"); // ID: 날짜 value = 0
+                timeEditor.commit();
+            }
+            // 시간재기
+            startTime = System.currentTimeMillis();
+            /*
+            else if(checkDate != null){
+                // 오늘 날짜는 dateTemp
+                // 현재시간 저장
+                date = System.currentTimeMillis();
+                nowDate = new Date(date);
+                sdf = new SimpleDateFormat("HH:mm"); // HH 시 mm 분으로 나타냄
+                String nowTime = sdf.format(nowDate); // 현재시간 스트링으로 변환
+
+                timeEditor.putString("startTime",nowTime);
+                timeEditor.commit();
+            }
+            */ // 삽질코드
 
             Widget.updateWidget(context, widgetManager, appWidgetId);
 
